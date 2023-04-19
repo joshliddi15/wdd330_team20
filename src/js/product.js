c147393e0f51d91b7a77ac302f5741be303105cb
jlassig <las22004@byui.edu>
2023-04-19 12:16:32 -0600

    worked on individual cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,20 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //First get what is already in localStorage and assign it to a variable (if there is anything, it will come back as an array):
+  let cart = getLocalStorage("so-cart");
+  //if there was no "cart" already, like you didn't have anything yet, then create an empty array:
+  if (!cart) {
+    cart = [];
+  }
+  //now we push the product that the user clicked on with "addtoCart" button to the end of the array
+  cart.push(product);
+  //and then setLocalStorage with the new contents of the cart:
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
