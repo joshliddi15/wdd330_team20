e3a2b1a059c7168dd23d1e8dadcc61d18354db7a
Brennon Jacobson <brennonj@me.com>
2023-04-19 20:03:21 -0700

    Fix Add to Cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,14 @@ import ProductData from './ProductData.mjs';
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cart = getLocalStorage("so-cart");
+
+  if (!cart) {
+    cart = [];
+  }
+
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
 
 // add to cart button event handler
