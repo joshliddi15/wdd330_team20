a2e178efe1431809e1444ec7f116eb67dbe509c0
Josh Liddiard <joshliddiard15@gmail.com>
2023-04-19 21:16:54 -0600

    Cart is functional

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart");
+  if (cart === null) {
+    cart = [];
+  }
+  cart.push(product);
+  console.log(typeof(cart));
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
