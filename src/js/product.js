73ffc5c99fab452afb2063e9f12fd735165e2fac
jlassig <las22004@byui.edu>
2023-04-26 20:06:46 -0600

    jkl-team2

--- src/js/product.js
+++ src/js/product.js
@@ -1,30 +1,22 @@
-
-import { setLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+// import { getLocalStorage } from './utils.mjs';
+import { setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
+const productId = getParam('product');
 
-function addProductToCart(product) {
-  //First get what is already in localStorage and assign it to a variable (if there is anything, it will come back as an array):
-  let cart = getLocalStorage("so-cart");
-  //if there was no "cart" already, like you didn't have anything yet, then create an empty array:
-  if (!cart) {
-    cart = [];
-  }
-  //now we push the product that the user clicked on with "addtoCart" button to the end of the array
-  cart.push(product);
-  //and then setLocalStorage with the new contents of the cart:
-  setLocalStorage("so-cart", cart);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   // const product = await dataSource.findProductById(e.target.dataset.id);
+//   ProductDetails.addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
